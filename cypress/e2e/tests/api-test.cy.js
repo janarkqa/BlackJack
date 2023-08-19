e44b8b1284b2a0313b5fb12bbbe86ead8e61ca16
Janar <janarkqa@gmail.com>
2023-08-19 00:39:58 -0500

    Update api-test.cy.js

--- cypress/e2e/tests/api-test.cy.js
+++ cypress/e2e/tests/api-test.cy.js
@@ -2,7 +2,6 @@ import successResponse from '../../fixtures/success-response.json'
 import tesdata from '../../fixtures/testdata.json'
 
 describe('API Tests', () => {
-  const playersCardsMap = new Map();
   let deckId
 
   before(function () {
@@ -30,27 +29,20 @@ describe('API Tests', () => {
   })
 
   it('Should deal three cards to each of two players', () => {
-    [1, 2].forEach(($element) => {
+    [1, 2].forEach((element) => {
       cy.request('GET', '/api/deck/' + deckId + '/draw/?count=3')
         .then((response) => {
           expect(response.status).to.eq(successResponse.status_code)
           expect(response.body.success).to.eq(successResponse.success)
           expect(response.body.deck_id).to.eq(deckId)
 
-          playersCardsMap.set($element, response.body.cards)
+          const player = calculateTotal(response.body.cards)
+
+          if (player === tesdata.blackjack) {
+            cy.log('Player' + element + ' has blackjack!')
+          }
         })
     })
-
-    const player1 = calculateTotal(playersCardsMap.get(1))
-    const player2 = calculateTotal(playersCardsMap.get(2))
-
-    if (player1 === tesdata.blackjack) {
-      cy.log('Player1 has blackjack!')
-    }
-
-    if (player2 === tesdata.blackjack) {
-      cy.log('Player2 has blackjack!')
-    }
   })
 })
 
@@ -64,6 +56,8 @@ function calculateTotal(cards) {
     } else if (card.value === 'ACE') {
       total += 11
       hasAce = true
+    } else if (card.value === 'JOKER') {
+      return tesdata.blackjack
     } else {
       total += parseInt(card.value)
     }
